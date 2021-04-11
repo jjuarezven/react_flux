497949d6c297dc62f4d9d4947eaf02ed10632039
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-04-11 14:00:43 -0500

    Adding courses page, rendering a table with courses

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import Header from './common/Header';
+import CoursesPage from './CoursesPage';
 
 function App() {
     function getPage() {
         const route = window.location.pathname;
-        if (route === '/about') {
-            return <AboutPage />
-        } else {
-            return <HomePage />
-        }
+        if (route === '/courses') return <CoursesPage />;
+        if (route === '/about') return <AboutPage />;
+        return <HomePage />;
     };
 
     // displays the header component and returns the getPage function result
