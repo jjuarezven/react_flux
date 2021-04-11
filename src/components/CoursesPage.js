41ac8713fa12d8ca4f06987d48d97ceaf794e0e2
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-04-11 14:26:56 -0500

    Refactoring class component to fucntion component and use hooks

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,45 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { getCourses } from "../api/courseApi";
 
 
+function CoursesPage() {
+    // state = {
+    //     courses: []
+    // };
+    const [courses, setCourses] = useState([]);
 
-class CoursesPage extends React.Component {
-    state = {
-        courses: []
-    };
+    useEffect(() => {
+        getCourses().then(_courses => setCourses(_courses));
+    }, []) // empty array tells React to run getCourses just one time
 
-    componentDidMount() {
-        getCourses().then(courses => this.setState({ courses : courses }));        
-    }
+    // componentDidMount() {
+    //     getCourses().then(courses => this.setState({ courses : courses }));        
+    // }
     
-
-    render() {
-        return (
-            <>
-                <h2>Courses</h2>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Author ID</th>
-                            <th>Category</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.courses.map(course => {
-                            return (
-                                <tr key={ course.id }>
-                                    <td>{ course.title }</td>
-                                    <td>{ course.authorId }</td>
-                                    <td>{ course.category }</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            </>
-        );
-    }
+    return (
+        <>
+            <h2>Courses</h2>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Author ID</th>
+                        <th>Category</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {courses.map(course => {
+                        return (
+                            <tr key={ course.id }>
+                                <td>{ course.title }</td>
+                                <td>{ course.authorId }</td>
+                                <td>{ course.category }</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </>
+    );
+    
 }
 
 export default CoursesPage;
