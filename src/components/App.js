392a3d17b1e57c9c308439834638f522ba29ef81
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-04-11 18:00:35 -0500

    Using React Router, passing parameters, link, navlink, redirect, switch

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,32 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import Header from './common/Header';
 import CoursesPage from './CoursesPage';
+import { Route, Switch, Redirect } from "react-router-dom";
+import NotFoundPage from './NotFoundPage.js';
+import ManageCoursePage from './ManageCoursePage.js';
+
 
 function App() {
-    function getPage() {
-        const route = window.location.pathname;
-        if (route === '/courses') return <CoursesPage />;
-        if (route === '/about') return <AboutPage />;
-        return <HomePage />;
-    };
+    // function getPage() {
+    //     const route = window.location.pathname;
+    //     if (route === '/courses') return <CoursesPage />;
+    //     if (route === '/about') return <AboutPage />;
+    //     return <HomePage />;
+    // };
 
     // displays the header component and returns the getPage function result
     return (
         <div className="containerFluid">
             <Header />
-            { getPage() }
+            <Switch>
+                <Route path='/' exact component={HomePage} />
+                <Route path='/courses' component={CoursesPage} />
+                <Route path='/about' component={AboutPage} />
+                <Route path='/course/:slug' component={ManageCoursePage} />
+                <Redirect from='/about-page' to='about' />
+                <Route component={NotFoundPage} />
+            </Switch>
+            { /*getPage()*/ }
         </div>
     );
 }
